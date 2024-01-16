0bf77773a2be76d97c0dd449384c80cc3ad7d4ea
soyUnaGata <fallen.snitch@gmail.com>
2024-01-16 12:33:30 +0200

    implement constants API

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
         <h1>index</h1>
-        <input type="text" class="border-2 border-orange-700">
+        <input v-model="city" type="text" class="border-2 border-orange-700">
         <NuxtLink :to="'/weather'">Go</NuxtLink>
     </div>
 </template>
+
+<script setup>
+
+const city = ref();
+
+</script>
 
