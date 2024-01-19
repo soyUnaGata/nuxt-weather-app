eaee084660fff9ca27cb5d7132ca666c8e2de312
soyUnaGata <fallen.snitch@gmail.com>
2024-01-19 12:51:58 +0200

    fix tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["tailwindcss"],
-  tailwindcss: {
-    exposeConfig: true,
+  modules: [],
+  css: ["./assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    `components/**/*.{vue,js,ts}`,
-    `layouts/**/*.vue`,
-    `pages/**/*.vue`,
-    `composables/**/*.{js,ts}`,
-    `plugins/**/*.{js,ts}`,
-    `utils/**/*.{js,ts}`,
-    `App.{js,ts,vue}`,
-    `app.{js,ts,vue}`,
-    `Error.{js,ts,vue}`,
-    `error.{js,ts,vue}`,
-    `app.config.{js,ts}`,
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
