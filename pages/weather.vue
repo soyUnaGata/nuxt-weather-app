1fd9094cc8ec7b674e1abb154b8a4f74c04b9169
soyUnaGata <fallen.snitch@gmail.com>
2024-01-22 18:18:35 +0200

    implement calendar icon

--- pages/weather.vue
+++ pages/weather.vue
@@ -47,6 +47,11 @@
 
             <div class="weather__details-date mt-4">
                 <div class="weather__details-date__numeric text-xl">
+                    <svg class="weather__details-calendar" xmlns="http://www.w3.org/2000/svg" height="20" width="20"
+                        viewBox="0 0 448 512">
+                        <path
+                            d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
+                    </svg>
                     {{ formatDay(timezone) }}th {{ formatMonth(timezone) }} '{{ formatYear(timezone) }}
                 </div>
                 <div class="weather__details-date__weekday text-base flex items-center gap-1.5 mt-2">
@@ -228,7 +233,8 @@ onMounted(getWeather)
     color: var(--primary);
 }
 
-.weather__details-city__icon {
+.weather__details-city__icon,
+.weather__details-calendar {
     fill: var(--primary);
 }
 
