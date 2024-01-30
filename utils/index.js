400b924da81ef12345c42dbe9516e9a8e4036d8d
soyUnaGata <fallen.snitch@gmail.com>
2024-01-30 19:50:18 +0200

    implement methods to convert pressure hpa to mm

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,11 @@
+const PRESSURE_UNIT = 0.750062;
+
 export const capitalizeFirstLetter = (str) => {
   if (!str) return "";
 
   return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const getPressureMm = (hpa) => {
+  return Math.round(hpa * PRESSURE_UNIT);
 };
