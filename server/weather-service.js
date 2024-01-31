21c5056120d4d5db43a5682fc78bd467f932702d
soyUnaGata <fallen.snitch@gmail.com>
2024-01-31 15:13:15 +0200

    amend structure

--- server/weather-service.js
+++ server/weather-service.js
@@ -1,25 +1,31 @@
+import axios from "axios";
+
 const API_KEY = "2e65091681075a7c367751f17ff53212";
-const FORECAST_URL = "https://api.openweathermap.org/data/2.5/weather";
+const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const FORECAST_KEY = "9394ab08719300551ef6070c2f9415cc";
 
 class ForecastService {
   async getForecast(city) {
     const url = `${FORECAST_URL}${city}&appid=${API_KEY}&units=imperial`;
-    return await api
+    return await axios
       .get(url)
       .then((response) => response.data)
       .then((info) => {
         if (!info) return [];
-        return info.map((forecast) => ({
-          dt: forecast.dt,
-          id: forecast.id,
-          main: forecast.main,
-          city: forecast.name,
-          about: forecast.sys,
-          timezone: forecast.timezone,
-          weather: forecast.weather,
-          wind: forecast.wind,
-        }));
+        console.log(info);
+        const forecast = info;
+        return {
+          country: forecast.city.country,
+          id: forecast.city.id,
+          city: forecast.city.name,
+          sunrize: forecast.city.sunrise,
+          sunset: forecast.city.sunset,
+          timezone: forecast.city.timezone,
+          details: forecast.list,
+        };
       });
   }
 }
+
+export default new ForecastService();
