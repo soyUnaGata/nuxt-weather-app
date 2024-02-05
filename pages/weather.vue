5cc58778298f18181177eac660213b2ce70c02a5
soyUnaGata <fallen.snitch@gmail.com>
2024-02-05 14:49:39 +0200

    amend template dynamic info

--- pages/weather.vue
+++ pages/weather.vue
@@ -31,7 +31,7 @@
                     <p v-if="isMetric" class="temperature__details-cel text-9xl flex">{{ fahrenheitToCelsius }}
                         <span class="temperature__details-cel__icon text-4xl">&deg;C</span>
                     </p>
-                    <p v-else class="temperature__details-fahr text-9xl flex"> {{ Math.round(temperature) }}
+                    <p v-else class="temperature__details-fahr text-9xl flex"> {{ Math.round(weather.temp) }}
                         <span class="temperature__details-fahr__icon text-4xl">&deg;F</span>
                     </p>
                 </div>
@@ -45,7 +45,7 @@
                                 <path
                                     d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
                             </svg>
-                            <h3 class="weather__details-city__name text-3xl"> {{ details.name }}, {{ details.sys?.country }}
+                            <h3 class="weather__details-city__name text-3xl"> {{ weather.city }}, {{ weather.country }}
                             </h3>
                         </div>
 
@@ -63,24 +63,25 @@
                                 <path
                                     d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
                             </svg>
-                            <span>{{ formatDay(timezone) }}th {{ formatMonth(timezone) }} '{{ formatYear(timezone) }}</span>
+                            <span>{{ formatDay(weather.timezone) }}th {{ formatMonth(weather.timezone) }} '{{
+                                formatYear(weather.timezone) }}</span>
                         </div>
                         <div class="weather__details-date__weekday text-base flex items-center gap-1.5 mt-2">
-                            {{ formatWeekday(timezone) }}
+                            {{ formatWeekday(weather.timezone) }}
                             <span class="pipeline border"></span>
-                            {{ formatTime(timezone) }}
+                            {{ formatTime(weather.timezone) }}
                         </div>
                     </div>
                 </section>
             </div>
 
             <div class="details flex w-full justify-center">
-                <section class="grid grid-cols-2 gap-y-5 gap-x-10">
+                <!-- <section class="grid grid-cols-2 gap-y-5 gap-x-10">
                     <sunrise :details="details" />
                     <humidity :details="details" />
                     <wind :winds="winds" />
                     <pressure :pressureToday="pressureNum" />
-                </section>
+                </section> -->
             </div>
         </main>
 
