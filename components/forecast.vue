630e847fe40e4b046a6afda85d03b74bbaec36bf
soyUnaGata <fallen.snitch@gmail.com>
2024-02-13 14:33:38 +0200

    style forecast-container

---
+++ components/forecast.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="forecast-container bg-gray-200/50 rounded-2xl pl-2.5 shadow-xl">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.forecast-container {
+    height: 210px;
+    width: 150px;
+}
+</style>
