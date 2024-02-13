7500da73bbe5f54e66bbc6c12be74cb7e65620ff
soyUnaGata <fallen.snitch@gmail.com>
2024-02-13 14:39:36 +0200

    implement some markup

--- components/forecast.vue
+++ components/forecast.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="forecast-container bg-gray-200/50 rounded-2xl pl-2.5 shadow-xl">
-
+        <div class="day-temperature"></div>
+        <div class="day-weather"></div>
+        <div class="day-date">
+            <h3 class="day-date__number"></h3>
+            <p class="day-date__day"></p>
+            <p class="day-date__hours"></p>
+        </div>
     </div>
 </template>
 
