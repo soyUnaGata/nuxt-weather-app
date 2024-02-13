a493d6fcdd02e50cc3549b5ae79150a282a287ab
soyUnaGata <fallen.snitch@gmail.com>
2024-02-13 14:33:22 +0200

    implement forecast component

--- pages/weather.vue
+++ pages/weather.vue
@@ -2,9 +2,16 @@
     <div class="wrapper">
         <nav>
             <ul class="flex justify-between">
-                <li>
-                    <NuxtLink to="/">Back</NuxtLink>
-                </li>
+                <NuxtLink to="/" class="flex items-center gap-3">
+                    <li>
+                        <svg class="fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="20"
+                            width="20">
+                            <path
+                                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                        </svg>
+                    </li>
+                    <p class="text-white">Return to search</p>
+                </NuxtLink>
                 <li>
                     <div class="toogle__temperature temp" id="button-toogler">
                         <input type="checkbox" class="toogle__checkbox" @click="toggleTemperature" />
@@ -87,7 +94,9 @@
 
 
 
-        <section>next</section>
+        <section class="forecast__for-days mt-16">
+            <forecast />
+        </section>
 
     </div>
 </template>
