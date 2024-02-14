6ceee07175b0c7741ac4cb2c09231d70e6b6a244
soyUnaGata <fallen.snitch@gmail.com>
2024-02-14 16:05:11 +0200

    implement template for temp

--- components/forecast.vue
+++ components/forecast.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="forecast-container bg-gray-200/50 rounded-2xl pl-2.5 shadow-xl" v-for="forecast in forecasts">
-        <div class="day-temperature">{{ forecast.main.temp }}</div>
+        <div class="day-temperature mt-6 font-thin">
+            <p v-if="isMetric" class="temperature__details-cel flex">{{ }}
+                <span class="temperature__details-cel__icon">&deg;C</span>
+            </p>
+            <p v-else class="temperature__details-fahr flex"> {{ forecast.main.temp }}
+                <span class="temperature__details-fahr__icon">&deg;F</span>
+            </p>
+        </div>
         <div class="day-weather">
             <img class="day-weather-img__sized" :src="`/img/state-weather/${forecast.weather[0].description}.png`"
                 :alt="`${forecast.weather[0].description}`">
         </div>
         <div class="day-date">
             <h3 class="day-date__number">{{ getDateTime(forecast.dt) }}</h3>
-            <p class="day-date__day">{{ getTime(forecast.dt) }}</p>
-            <p class="day-date__hours"></p>
+            <p class="day-date__hours">{{ getTime(forecast.dt) }}</p>
         </div>
     </div>
 </template>
