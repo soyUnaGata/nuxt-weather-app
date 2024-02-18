8e68820e8c4a54e534da0968d46a95be350822df
soyUnaGata <fallen.snitch@gmail.com>
2024-02-18 19:18:40 +0200

    init swiper

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [],
+  modules: ["nuxt-swiper"],
   css: ["./assets/css/main.css"],
   postcss: {
     plugins: {
