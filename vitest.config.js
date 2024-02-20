9f66162169e74784390db796d0ff71e05f3fbd74
soyUnaGata <fallen.snitch@gmail.com>
2024-02-20 20:06:14 +0200

    amend config

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,10 @@
-import { defineVitestConfig } from "@nuxt/test-utils/config";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-export default defineVitestConfig({
+export default defineConfig({
+  plugins: [vue()],
   test: {
-    environment: "nuxt",
+    globals: true,
+    environment: "jsdom",
   },
 });
