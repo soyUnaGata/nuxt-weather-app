17624c6cded6ea505ccb208ebfb7a321416cd565
soyUnaGata <fallen.snitch@gmail.com>
2024-02-22 14:31:43 +0200

    implement title

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: 'Nalssi',
+  meta: [
+    { name: 'Weather and forecast', content: 'Find out your sunny day' }
+  ]
+})
+</script>
